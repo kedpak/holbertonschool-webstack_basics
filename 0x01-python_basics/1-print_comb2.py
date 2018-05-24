beb83151bab07eec2ff18a403f28a274dd7f4898
Kevin Pak <kedpak@yahoo.com>
2018-05-24 15:51:47 -0700

    remove variable with number

--- 0x01-python_basics/1-print_comb2.py
+++ 0x01-python_basics/1-print_comb2.py
@@ -8,8 +8,7 @@ Print numbers from 0 to 99
 def print_comb2():
     ''' print numbers '''
 
-    i = 0
-    while i < 100:
+    for i in range(100):
         if i == 99:
             print ("%02d" % (i,))
         else:
