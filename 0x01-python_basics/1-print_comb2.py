ce59a124ac101c1fbc6b23a03e5b73a003a17d82
Kevin Pak <kedpak@yahoo.com>
2018-05-24 16:02:53 -0700

    Remove methods

--- 0x01-python_basics/1-print_comb2.py
+++ 0x01-python_basics/1-print_comb2.py
@@ -4,16 +4,9 @@
 Print numbers from 0 to 99
 '''
 
-
-def print_comb2():
-    ''' print numbers '''
-
-    for i in range(100):
-        if i == 99:
-            print ("%02d" % (i,))
-        else:
-            print ("%02d" % (i,), end=', ')
-        i += 1
-
-
-print_comb2()
+for i in range(100):
+    if i == 99:
+        print ("%02d" % (i,))
+    else:
+        print ("%02d" % (i,), end=', ')
+    i += 1
