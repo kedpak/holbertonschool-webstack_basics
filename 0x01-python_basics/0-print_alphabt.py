4ca25492c373e1c9cf7df868bb489652ac32f127
Kevin Pak <kedpak@yahoo.com>
2018-05-24 15:41:50 -0700

    Place inside method

--- 0x01-python_basics/0-print_alphabt.py
+++ 0x01-python_basics/0-print_alphabt.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
-
-
 '''
 print alaphabet except q and e
 '''
-c = 97
-i = 0
-while i < 26:
-    if c == 101 or c == 113:
-        pass
-    else:
-        print(chr(c), end='')
-    c += 1
-    i += 1
+
+def printAlpha():
+    '''print alphabet'''
+    c = 97
+    i = 0
+    while i < 26:
+        if c == 101 or c == 113:
+            pass
+        else:
+            print(chr(c), end='')
+        c += 1
+        i += 1
+
+printAlpha()
