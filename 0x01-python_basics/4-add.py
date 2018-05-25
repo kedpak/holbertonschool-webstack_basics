5a7df281b8cb0bdf8d1b15332a40e6f3efbe007c
Kevin Pak <kedpak@yahoo.com>
2018-05-24 17:27:29 -0700

    Implement name guard

--- 0x01-python_basics/4-add.py
+++ 0x01-python_basics/4-add.py
@@ -5,6 +5,7 @@ from add_4 import add
 add two nums
 '''
 
-a = 1
-b = 2
-print("{} + {} = {}".format(a, b, add(a, b)))
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
