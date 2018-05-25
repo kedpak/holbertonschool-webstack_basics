a309831e26828115b585eee1277a58406ff74f51
Kevin Pak <kedpak@yahoo.com>
2018-05-24 17:37:20 -0700

    first commit

---
+++ 0x01-python_basics/5-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+'''
+print number of arguments 
+'''
+import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        print ("{} arguments.".format(len(sys.argv) - 1))
+    elif len(sys.argv) > 1:
+        print ("{} arguments:".format(len(sys.argv) - 1))
+        for i in range(len(sys.argv)):
+            if i > 0:
+                print ("{}: {}".format(i, sys.argv[i]))
