ee97933bf20e424275e4d3ac874c77885164529d
Kevin Pak <kedpak@yahoo.com>
2018-05-27 21:25:30 -0700

    change format

--- 0x01-python_basics/101-print_diagonal.py
+++ 0x01-python_basics/101-print_diagonal.py
@@ -7,19 +7,8 @@ print diagonals
 def print_diagonal(n):
     """Print diagonals based on n"""
     if n <= 0:
-        print('\n')
-        return
-    i = 0
-    j = 0
-    count = 1
-    while i < n:
-        while j < count:
-            if j + 1 == count:
-                print('\\')
-            else:
-                print(' ', end='')
-            j += 1
-        count += 1
-        i += 1
-        j = 0
-    return
+        print()
+    else:
+        for i in range(n):
+            print(i * ' ' + '\\')
+        print()
