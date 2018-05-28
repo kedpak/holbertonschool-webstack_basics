14af672282125a6baf7ee1284453048afb6e79b0
Kevin Pak <kedpak@yahoo.com>
2018-05-27 21:08:54 -0700

    first commit

---
+++ 0x01-python_basics/101-print_diagonal.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+print diagonals
+"""
+
+
+def print_diagonal(n):
+    """Print diagonals based on n"""
+    if n <= 0:
+        print('\n')
+    i = 0
+    j = 0
+    count = 1
+    while i < n:
+        while j < count:
+            if j + 1 == count:
+                print('\\')
+            else:
+                print(' ', end='')
+            j += 1
+        count += 1
+        i += 1
+        j = 0
